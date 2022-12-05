426b85764a98777c16d4a14e0f311afb49ccbf02
Kamal MEHDI <operabyte>
2022-12-05 22:11:33 +0000

    Add payment config

--- bleme/api-payment/overlays/aws-prod/files/config/production.js
+++ bleme/api-payment/overlays/aws-prod/files/config/production.js
@@ -15,5 +15,12 @@ module.exports = {
       password: 'postgres',
       port:     5432
     }
+  },
+  payment: {
+    stripe: {
+      publicKey: "pk_test_51M8PBwAawPHCrdJRe2wBTwacicYLurQawtSBrCOrxeV1gZAYKRJfaFm8Sg9AM6biGGqBTQexydBZAeMBoP5LypiD00ZYUMOhMu",
+      secretKey: "sk_test_51M8PBwAawPHCrdJRplwU4ECUMfMtXGRPGPkdqbO3rdKxrfAZMX3HU6ODFh50wnR39LPwiK7aB1NqvxeULV7CE1kO00dp4Ik5hH",
+      apiVersion: "2022-11-15"
+    }
   }
 }
